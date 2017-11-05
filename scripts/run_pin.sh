a99ba676bc85099267596eb007b74d4db6713a88
Tian Tan <ttan@vagabond.(none)>
2017-11-05 14:54:26 -0600

    update run_pin script to run gemm test

--- scripts/run_pin.sh
+++ scripts/run_pin.sh
@@ -1,3 +1,3 @@
 set -e
 export OMP_NUM_THREADS=1
-~/Tools/pin-3.4-97438-gf90d1f746-gcc-linux/pin -t ~/Tools/pin-3.4-97438-gf90d1f746-gcc-linux/source/tools/MyPinTool/obj-intel64/procatrace.so -- python benchmark/bench_linear.py -ns 100 -nf 10
+$PIN_ROOT/pin -t pintools/obj-intel64/procatrace.so -- python benchmark/bench_gemm.py
